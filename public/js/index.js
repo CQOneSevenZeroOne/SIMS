13fef028628f916a95a98b1275edb439887f0e32
xiaotang66 <862115203@qq.com>
2018-01-04 11:57:49 +0800

    登录成功后跳转页面

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 $(function(){
+
     $("#sub").on('click',function(){
+        
         $.ajax({
             url:'http://10.40.153.145:8888/login',
             type:'post',
@@ -8,10 +10,26 @@ $(function(){
                 password:$('#password').val()
             },
             success:function(data){
+                 setcookie();
                 if(data==='fail'){
                     $('#err').css('display','block')
                 }
-            }
+                if(data==='salesman'){
+
+                    location.href="html/salesman.html";
+                }
+                if(data==='admin')
+                {
+                   location.href="html/admin.html";
+                }
+            } 
         })
+        function setcookie(){
+            var obj={};
+            obj[$('#accound').val()]=$('#password').val();
+            console.log(obj)
+             $.cookie.raw = true;
+             $.cookie("user",JSON.stringify(obj), { path: "/", expires:7});
+        }
     })
 })
