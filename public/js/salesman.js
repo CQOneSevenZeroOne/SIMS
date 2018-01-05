54bcb2b8d3e8ec41a0b1660536a2434c83e2dca4
hch <577031133@qq.com>
2018-01-05 19:09:46 +0800

    提交

--- public/js/salesman.js
+++ public/js/salesman.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
     getcookie();
 
@@ -9,14 +8,4 @@ $(function() {
         }
 
     }
-=======
-$(function(){
-	getcookie();
-	function getcookie(){
-	 var obj1=JSON.parse($.cookie("user"));s
-	 for(var i in obj1){
-	 	$(".username").html(i)
-	 }
-	}
->>>>>>> 676480f82ed0a36fc3586c0c7283d7719872c2f2
 })
