f5b5b4b085bbcc1d78a3d4a469ab4c07cba3834b
hch <577031133@qq.com>
2018-01-05 11:42:40 +0800

    tijiao

--- public/js/salesman.js
+++ public/js/salesman.js
@@ -1,10 +1,11 @@
-$(function(){
-	getcookie();
-	function getcookie(){
-	 var obj1=JSON.parse($.cookie("user"));s
-	 for(var i in obj1){
-	 	$(".username").html(i)
-	 }
-	 
-	}
+$(function() {
+    getcookie();
+
+    function getcookie() {
+        var obj1 = JSON.parse($.cookie("user"));
+        for (var i in obj1) {
+            $(".username").html(i)
+        }
+
+    }
 })
